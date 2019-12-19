a37e74049bd1febd4e1bbe15c737855633c57b87
张举 <zhangju@aspirecn.com>
2019-12-19 20:44:55 +0800

    [添加] 添加 静态文件

--- src/main/java/com/zhangju/manage/controller/IndexController.java
+++ src/main/java/com/zhangju/manage/controller/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author zhangju 卓望信息(北京)
  * @version 1.0
@@ -15,8 +17,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Slf4j
 public class IndexController {
     @RequestMapping("/")
-    public ModelAndView index() {
+    public ModelAndView index(HttpServletRequest request) {
+        StringBuffer requestURL = request.getRequestURL();
+        System.err.println(requestURL);
+
         ModelAndView modelAndView = new ModelAndView("index");
+
+
         return modelAndView;
     }
 }
