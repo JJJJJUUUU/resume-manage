7fb76f4f186e6abc52e7e7d6e1b15e225c509816
张举 <zhangju@aspirecn.com>
2019-12-20 10:14:52 +0800

    [修改] 修改登录

---
+++ src/main/java/com/zhangju/manage/common/JSONObjectResult.java
@@ -0,0 +1,28 @@
+package com.zhangju.manage.common;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
+
+/**
+ * @author zhangju 卓望信息(北京)
+ * @version 1.0
+ * @since 1.0
+ * 2019/12/20 9:16
+ **/
+@Data
+public class JSONObjectResult {
+    /**
+     *返回码
+     */
+    private int code;
+
+    /**
+     *消息
+     */
+    private String message;
+
+    /**
+     *返回数据
+     */
+    private JSONObject data;
+}
--- src/main/java/com/zhangju/manage/controller/IndexController.java
+++ src/main/java/com/zhangju/manage/controller/IndexController.java
@@ -1,8 +1,11 @@
 package com.zhangju.manage.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.zhangju.manage.common.JSONObjectResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,14 +19,30 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @Slf4j
 public class IndexController {
-    @RequestMapping("/")
+    @RequestMapping({"/","/index"})
     public ModelAndView index(HttpServletRequest request) {
         StringBuffer requestURL = request.getRequestURL();
-        System.err.println(requestURL);
-
         ModelAndView modelAndView = new ModelAndView("index");
-
-
         return modelAndView;
+    }
+
+    @RequestMapping("/login")
+    public ModelAndView login(HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView("login");
+        return modelAndView;
+    }
+
+    @RequestMapping("/login_admin")
+    @ResponseBody
+    public JSONObjectResult loginAdmin(HttpServletRequest request, String userName, String password) {
+
+        log.info("userName:{}",userName);
+        log.info("password:{}",password);
+
+        JSONObjectResult result = new JSONObjectResult();
+        result.setCode(0);
+
+
+        return result;
     }
 }
